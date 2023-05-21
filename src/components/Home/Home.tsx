e00fd83a3ada78cb3be4d7992129916d052fb9f6
thanhngan22 <nganbui9a2017@gmail.com>
2023-05-21 17:30:14 +0700

    add tailwindcss

---
+++ src/components/Home/Home.tsx
@@ -0,0 +1,20 @@
+
+
+export default function Home() {
+    return (
+        <main>
+            <div className="container">
+                <div className="main__app">
+
+                </div>
+                <div className="description">
+
+                </div>
+                <div className="footer">
+                    
+                </div>
+
+            </div>
+        </main>
+    )
+}
