2e66eedcdf9d5dfe2b13b3fb2bfd47a2fe154f6c
thanhngan22 <nganbui9a2017@gmail.com>
2023-05-21 17:32:33 +0700

    config tailwind

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -4,14 +4,14 @@ export default function Home() {
     return (
         <main>
             <div className="container">
-                <div className="main__app">
-
+                <div className="main__app bg-pink-200 w-80 h-full">
+                    Home
                 </div>
                 <div className="description">
 
                 </div>
                 <div className="footer">
-                    
+
                 </div>
 
             </div>
